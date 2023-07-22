f9052a3dc9d1ec2ffa6facd346bed03d3c8e2db7
Rishat_Imangulov <gr40in@yandex.ru>
2023-07-23 03:11:36 +0500

    @

--- model/Note.py
+++ model/Note.py
@@ -1,12 +1,47 @@
-from abc import ABC
+import datetime
+import itertools
+
 
 class Note(object):
-    def __init__(self, id, date, title, text):
-        self.id = id
-        self.date = date
-        self.title = title
-        self.text = text
+    __id_iter = itertools.count(1)
+    __id: int
+    __data: datetime
+    __title: str
+    __text: str
 
+    def __init__(self, date, title, text):
+        self.__id = next(Note.__id_iter)
+        self.__date = date
+        self.__title = title
+        self.__text = text
 
+    @property
+    def id(self):
+        return self.__id
 
+    @property
+    def date(self):
+        return datetime.datetime.strftime(self.__date, '%d.%m.%Y %H:%M')
 
+    @date.setter
+    def date(self, date):
+        self.__date = date
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, title):
+        self.__title = title
+
+    @property
+    def text(self):
+        return self.__text
+
+    @text.setter
+    def text(self, text):
+        self.__text = text
+
+    def __str__(self) -> str:
+        return '№' + str(self.id) + ' << ' + self.title + ' >> ' + self.date + '\n' + self.text
